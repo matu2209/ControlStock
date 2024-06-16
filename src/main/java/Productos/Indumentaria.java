c5470093058b1d052e11db638aa6b638b4f1c14d
tomasblacke <tomaseblack@outlook.com>
2024-06-16 20:43:34 -0300

    integre, sin probar, las interfaces con el menu principal

--- src/main/java/Productos/Indumentaria.java
+++ src/main/java/Productos/Indumentaria.java
@@ -1,5 +1,14 @@
 package Productos;
 
+import enumeradores.Prioridad;
+import enumeradores.Segmento;
+
 public class Indumentaria extends Producto{
+
+
+    public Indumentaria(String marca, String articulo, Integer talle, Integer stock, Double volumen, Prioridad prioridad) {
+        super(marca, articulo, talle, stock, volumen, prioridad);
+
+    }
 
 }
