e0cb59b772d7eed084cd56fa0aaefa2d007d3e8a
matu2209 <matu.2209@gmail.com>
2024-06-18 12:54:07 -0300

    Se agrega implementacion clases accesorio e indumentaria.

--- src/main/java/Productos/Accesorios.java
+++ src/main/java/Productos/Accesorios.java
@@ -1,5 +1,19 @@
 package Productos;
 
+import enumeradores.Disciplina;
+import enumeradores.Prioridad;
+
 public class Accesorios extends Producto{
+
+    private Disciplina disciplina;
+
+    public Accesorios(String marca, String articulo, Integer talle, Integer stock, Double volumen, Prioridad prioridad, Disciplina disciplina) {
+        super(marca,articulo,talle,stock,volumen,prioridad);
+        this.disciplina = disciplina;
+    }
+
+    public Disciplina getDisciplina() {
+        return disciplina;
+    }
 
 }
